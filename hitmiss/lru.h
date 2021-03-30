3213ec7a93c26a4cc1c07488f72964f5c1923c8b
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2021-03-30 20:41:36 +0300

    Change LRU interface to take in a cache set

--- hitmiss/lru.h
+++ hitmiss/lru.h
@@ -2,6 +2,7 @@
 #define LRU_H
 
 #include "bitarray.h"
+#include "cacheset.h"
 
 #include <cassert>
 #include <cmath>
@@ -11,29 +12,38 @@ class LRU
 public:
 	// TODO: perhaps, we could simply pass in the associativity?
 	template <class CacheAccessParams>
-	std::size_t getReplacementBitLength(const CacheAccessParams& accessParams) const
+	std::size_t getReplacementBitLengthPerLine(const CacheAccessParams& accessParams) const
 	{
 		return static_cast<std::size_t>(std::ceil(std::log2(accessParams.getAssociativity())));
 	}
 
-	template <typename Iterator>
-	Iterator evict(Iterator first, Iterator last) const
+	template <class CacheAccessParams>
+	std::size_t getReplacementBitLengthPerSet(const CacheAccessParams& accessParams) const
 	{
-		assert(first < last);
-		auto n = last - first;
-		
-		Iterator evicted;
-		for (;first != last; ++first)
+		return 0;
+	}
+
+
+	template <class CacheAccessParams>
+	std::size_t evict(CacheSet<CacheAccessParams, LRU>& cacheSet) const
+	{
+		auto n = cacheSet.getSize();
+		std::size_t evicted = n;
+
+		//for (; first != last; ++first)
+		for (std::size_t i = 0; i < cacheSet.getSize(); ++i)
 		{
-			if (!first->isValid())
+			auto& cacheLine = cacheSet[i];
+
+			if (!cacheLine.isValid())
 				continue;
 
-			auto bits = first->getReplacementBits();
+			auto bits = cacheLine.getReplacementBits();
 			auto rank = bits.toNumber<decltype(n)>();
 
 			if (rank == 0)
 			{
-				evicted = first;
+				evicted = i;
 				bits.setNumber(n-1);
 			}
 			else
@@ -41,102 +51,119 @@ public:
 				--rank;
 				assert(rank >= 0);
 				bits.setNumber(rank);
-				//bits.decrement();
 			}
-			
-			first->setReplacementBits(bits);
+					
+			cacheLine.setReplacementBits(bits);
 		}	// for
-
-		/*
-		Iterator evicted;
-		while (first != last)
-		{
-			first->updateReplacementBits([n, &first, &evicted](auto p, auto q) 
-			{
-				auto rank = BitArray::getNumber<decltype(n)>(p, q);
-
-				if (rank == 0)
-				{
-					evicted = first;
-					BitArray::setNumber(n - 1, p, q);
-				}
-				else
-				{
-					--rank;
-					assert(rank >= 0);
-					//bits.setNumber(rank);
-					BitArray::setNumber(rank, p, q);
-				}
-			});	// updateReplacementBits
-
-			
-			//first->setReplacementBits(bits);
-			++first;
-		}
-		*/
-
-		assert(evicted != last);
+						
+		assert(evicted < cacheSet.getSize());
 		return evicted;
-	}
+	}	// evict
 
-	template <typename Iterator>
-	void update(Iterator updatedIt, Iterator first, Iterator last) const
+	//template <typename Iterator>
+	//Iterator evict(Iterator first, Iterator last) const
+	//{
+	//	assert(first < last);
+	//	auto n = last - first;
+	//	
+	//	Iterator evicted;
+	//	for (;first != last; ++first)
+	//	{
+	//		if (!first->isValid())
+	//			continue;
+
+	//		auto bits = first->getReplacementBits();
+	//		auto rank = bits.toNumber<decltype(n)>();
+
+	//		if (rank == 0)
+	//		{
+	//			evicted = first;
+	//			bits.setNumber(n-1);
+	//		}
+	//		else
+	//		{
+	//			--rank;
+	//			assert(rank >= 0);
+	//			bits.setNumber(rank);
+	//			//bits.decrement();
+	//		}
+	//		
+	//		first->setReplacementBits(bits);
+	//	}	// for
+	//			
+	//	assert(evicted != last);
+	//	return evicted;
+	//}
+
+	//template <typename Iterator>
+	//void update(Iterator updatedIt, Iterator first, Iterator last) const
+	//{
+	//	assert(first < last);
+
+	//	auto n = last - first;
+
+	//	auto ubits = updatedIt->getReplacementBits();
+	//	auto urank = ubits.toNumber<decltype(n)>();
+	//	//if (rank == n-1)
+	//	for (; first != last; ++first)
+	//	{
+	//		// It looks like we have to update LRU counters even for invalid entries:
+	//		// https://classroom.udacity.com/courses/ud007/lessons/632538649/concepts/9391386340923
+	//		//if (!first->isValid() || first == updatedIt)
+	//		if (first == updatedIt)
+	//			continue;
+
+	//		auto bits = first->getReplacementBits();
+	//		auto rank = bits.toNumber<decltype(n)>();
+	//		if (rank > urank)
+	//		//if (bits > ubits)
+	//		{
+	//			--rank;
+	//			assert(rank >= 0);
+	//			bits.setNumber(rank);
+	//			//bits.decrement();
+	//			first->setReplacementBits(bits);
+	//		}
+	//	}	// while
+
+	//	ubits.setNumber(n-1);
+	//	updatedIt->setReplacementBits(ubits);			
+	//}	// update
+
+	template <class CacheAccessParams>
+	void update(CacheSet<CacheAccessParams, LRU> &cacheSet, std::size_t updatedIndex) const
 	{
-		assert(first < last);
-
-		auto n = last - first;
-
-		auto ubits = updatedIt->getReplacementBits();
+		auto n = cacheSet.getSize();
+		auto ubits = cacheSet[updatedIndex].getReplacementBits();
 		auto urank = ubits.toNumber<decltype(n)>();
 		//if (rank == n-1)
-		for (; first != last; ++first)
+		//for (; first != last; ++first)
+		for (std::size_t i = 0; i < cacheSet.getSize(); ++i)
 		{
 			// It looks like we have to update LRU counters even for invalid entries:
 			// https://classroom.udacity.com/courses/ud007/lessons/632538649/concepts/9391386340923
 			//if (!first->isValid() || first == updatedIt)
-			if (first == updatedIt)
+			//if (first == updatedIt)
+			if (i == updatedIndex)
 				continue;
 
-			auto bits = first->getReplacementBits();
+			auto bits = cacheSet[i].getReplacementBits();
 			auto rank = bits.toNumber<decltype(n)>();
 			if (rank > urank)
-			//if (bits > ubits)
 			{
 				--rank;
 				assert(rank >= 0);
 				bits.setNumber(rank);
 				//bits.decrement();
-				first->setReplacementBits(bits);
+				cacheSet[i].setReplacementBits(bits);
 			}
 		}	// while
 
-		ubits.setNumber(n-1);
-		updatedIt->setReplacementBits(ubits);
-		
-
-		/*
-		updatedIt->updateReplacementBits([n, &first, &last](auto p, auto q)
-			{
-				auto urank = BitArray::getNumber<decltype(n)>(p, q);
-				
-				while (first != last)
-				{
-					first->updateReplacementBits([urank](auto r, auto t) 
-						{
-							if (rank > urank)
-							{
-								--rank;
-								assert(rank >= 0);
-								BitArray::setNumber(rank, r, t);
-							}
-						});	// updateReplacementBits inner
-
-					BitArray::setNumber(n-1, p, q);
-				}
-			});	// updateReplacementBits outer
-			*/
+		ubits.setNumber(n - 1);
+		cacheSet[updatedIndex].setReplacementBits(ubits);
 	}	// update
-};
+
+};	// LRU
 
 
 
